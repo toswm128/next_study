f6edeee924f8f6d8a145e3ae12c8be764af80f8c
조민수 <chominsu10@gmail.com>
2021-09-06 16:46:58 +0900

    [ADD] getInitialProps

--- next/pages/home.jsx
+++ next/pages/home.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { useState } from "react";
 
-const _home = () => {
+const Home = ({ res }) => {
   const [value, setValue] = useState("");
-  const [list, setList] = useState(["a", "b"]);
+  const [list, setList] = useState(res);
 
   return (
     <div>
+      <h1>hello</h1>
       <input
         type="text"
         value={value}
@@ -19,17 +20,21 @@ const _home = () => {
       >
         제출
       </button>
-      <div>hello</div>
-      {list &&
-        list.map(current => {
-          return (
-            <div>
-              <Link href={"/about/" + current}>{current}</Link>
-            </div>
-          );
-        })}
+      {list.map(current => {
+        return (
+          <div>
+            <Link href={"/about/" + current}>{current}</Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default _home;
+Home.getInitialProps = () => {
+  // context.query.id = 'test'
+  const res = ["hello", "myNameIs", "minsu"];
+  return { res };
+};
+
+export default Home;
