4c3c08afad620ff6a04c2b993d0c32a2f67338f4
조민수 <chominsu10@gmail.com>
2021-09-07 11:12:56 +0900

    [FIX] 페이지 이동 될때 에러
    페이지 이동 될때 store가 초기화되어 에러가 뜨는 현상 _app.js에서 store를 state를 이용함으로써 해결

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,9 +1,10 @@
 import "../styles/globals.css";
 import { Provider } from "mobx-react";
 import Store from "../Store/Store";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const store = new Store();
+  const [store, setStore] = useState(new Store());
 
   return (
     <Provider store={store}>
